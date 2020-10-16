73595327b4687563a525821cae20199e18bf2384
Nicolas Gnyra <nicolas@gnyra.com>
2020-10-15 22:40:39 -0400

    Bump to version 0.3.2

--- Source/DynamicOpenVR.BeatSaber/Properties/AssemblyInfo.cs
+++ Source/DynamicOpenVR.BeatSaber/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("1a5be41d-60b4-4185-97c1-78c4a78354b5")]
 
-[assembly: AssemblyVersion("0.3.1.0")]
-[assembly: AssemblyFileVersion("0.3.1.0")]
+[assembly: AssemblyVersion("0.3.2.0")]
+[assembly: AssemblyFileVersion("0.3.2.0")]

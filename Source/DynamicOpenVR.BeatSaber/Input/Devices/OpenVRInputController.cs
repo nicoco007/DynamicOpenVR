637a8ecfa47c66d2ea7c5c9837909b1567b92d80
Nicolas Gnyra <nicolas@gnyra.com>
2024-12-16 18:21:52 -0500

    Add more inputs to controller

--- Source/DynamicOpenVR.BeatSaber/Input/Devices/OpenVRInputController.cs
+++ Source/DynamicOpenVR.BeatSaber/Input/Devices/OpenVRInputController.cs
@@ -18,6 +18,7 @@
 
 using DynamicOpenVR.BeatSaber.InputCollections;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.Layouts;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.InputSystem.XR;
@@ -25,20 +26,103 @@ using UnityEngine.XR;
 
 namespace DynamicOpenVR.BeatSaber.Input.Devices
 {
-    [InputControlLayout(stateType = typeof(OpenVRInputControllerState))]
+    [InputControlLayout(stateType = typeof(OpenVRInputControllerState), commonUsages = new string[] { "LeftHand", "RightHand" })]
     internal class OpenVRInputController : XRController, IInputUpdateCallbackReceiver
     {
         private UnityXRActionsHand _actions;
 
+        public ButtonControl system { get; private set; }
+
+        public ButtonControl systemTouched { get; private set; }
+
+        public ButtonControl primaryButton { get; private set; }
+
+        public ButtonControl primaryTouched { get; private set; }
+
+        public ButtonControl secondaryButton { get; private set; }
+
+        public ButtonControl secondaryTouched { get; private set; }
+
+        public AxisControl grip { get; private set; }
+
+        public ButtonControl gripPressed { get; private set; }
+
+        public AxisControl gripForce { get; private set; }
+
+        public AxisControl trigger { get; private set; }
+
+        public ButtonControl triggerPressed { get; private set; }
+
+        public ButtonControl triggerTouched { get; private set; }
+
+        public Vector2Control thumbstick { get; private set; }
+
+        public ButtonControl thumbstickClicked { get; private set; }
+
+        public ButtonControl thumbstickTouched { get; private set; }
+
+        public Vector2Control trackpad { get; private set; }
+
+        public ButtonControl trackpadTouched { get; private set; }
+
+        public AxisControl trackpadForce { get; private set; }
+
+        public PoseControl devicePose { get; private set; }
+
+        public PoseControl pointer { get; private set; }
+
+        public new ButtonControl isTracked { get; private set; }
+
+        public new IntegerControl trackingState { get; private set; }
+
+        public new Vector3Control devicePosition { get; private set; }
+
+        public new QuaternionControl deviceRotation { get; private set; }
+
+        public Vector3Control pointerPosition { get; private set; }
+
+        public QuaternionControl pointerRotation { get; private set; }
+
         public void OnUpdate()
         {
             bool isTracked = _actions.pose.isTracking;
+            InputTrackingState trackingState = isTracked ? InputTrackingState.Position | InputTrackingState.Rotation | InputTrackingState.Velocity | InputTrackingState.AngularVelocity : InputTrackingState.None;
             var state = new OpenVRInputControllerState()
             {
+                system = default,
+                systemTouched = default,
+                primaryButton = _actions.primaryButton.state,
+                primaryTouched = _actions.primaryTouch.state,
+                secondaryButton = _actions.secondaryButton.state,
+                secondaryTouched = _actions.secondaryTouch.state,
+                grip = _actions.grip.value,
+                gripPressed = _actions.gripButton.state,
+                gripForce = default,
+                trigger = _actions.trigger.value,
+                triggerPressed = _actions.triggerButton.state,
+                triggerTouched = default,
+                thumbstick = _actions.primary2DAxis.vector,
+                thumbstickClicked = _actions.primary2DAxisClick.state,
+                thumbstickTouched = _actions.primary2DAxisTouch.state,
+                trackpad = default,
+                trackpadTouched = default,
+                trackpadForce = default,
+                devicePose = new PoseState()
+                {
+                    isTracked = isTracked,
+                    trackingState = trackingState,
+                    position = _actions.pose.position,
+                    rotation = _actions.pose.rotation,
+                    velocity = _actions.pose.velocity,
+                    angularVelocity = _actions.pose.angularVelocity,
+                },
+                pointer = default,
                 isTracked = isTracked,
-                trackingState = isTracked ? InputTrackingState.Position | InputTrackingState.Rotation | InputTrackingState.Velocity | InputTrackingState.AngularVelocity : InputTrackingState.None,
+                trackingState = trackingState,
                 devicePosition = _actions.pose.position,
                 deviceRotation = _actions.pose.rotation,
+                pointerPosition = default,
+                pointerRotation = default,
             };
 
             InputSystem.QueueStateEvent(this, state);
@@ -47,6 +131,7 @@ namespace DynamicOpenVR.BeatSaber.Input.Devices
         public override void FinishSetup()
         {
             base.FinishSetup();
+
             var descriptor = XRDeviceDescriptor.FromJson(description.capabilities);
             if (descriptor != null)
             {
@@ -59,6 +144,29 @@ namespace DynamicOpenVR.BeatSaber.Input.Devices
                     _actions = Plugin.unityXRActions.right;
                 }
             }
+
+            system = GetChildControl<ButtonControl>("system");
+            systemTouched = GetChildControl<ButtonControl>("systemTouched");
+            primaryButton = GetChildControl<ButtonControl>("primaryButton");
+            primaryTouched = GetChildControl<ButtonControl>("primaryTouched");
+            secondaryButton = GetChildControl<ButtonControl>("secondaryButton");
+            secondaryTouched = GetChildControl<ButtonControl>("secondaryTouched");
+            grip = GetChildControl<AxisControl>("grip");
+            gripPressed = GetChildControl<ButtonControl>("gripPressed");
+            gripForce = GetChildControl<AxisControl>("gripForce");
+            trigger = GetChildControl<AxisControl>("trigger");
+            triggerPressed = GetChildControl<ButtonControl>("triggerPressed");
+            triggerTouched = GetChildControl<ButtonControl>("triggerTouched");
+            thumbstick = GetChildControl<Vector2Control>("thumbstick");
+            thumbstickClicked = GetChildControl<ButtonControl>("thumbstickClicked");
+            thumbstickTouched = GetChildControl<ButtonControl>("thumbstickTouched");
+            trackpad = GetChildControl<Vector2Control>("trackpad");
+            trackpadTouched = GetChildControl<ButtonControl>("trackpadTouched");
+            trackpadForce = GetChildControl<AxisControl>("trackpadForce");
+            devicePose = GetChildControl<PoseControl>("devicePose");
+            pointer = GetChildControl<PoseControl>("pointer");
+            pointerPosition = GetChildControl<Vector3Control>("pointerPosition");
+            pointerRotation = GetChildControl<QuaternionControl>("pointerRotation");
         }
     }
 }
--- Source/DynamicOpenVR.BeatSaber/Input/Devices/OpenVRInputControllerState.cs
+++ Source/DynamicOpenVR.BeatSaber/Input/Devices/OpenVRInputControllerState.cs
@@ -21,24 +21,104 @@ using UnityEngine;
 using UnityEngine.InputSystem.Layouts;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.InputSystem.Utilities;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.XR;
 
 namespace DynamicOpenVR.BeatSaber.Input.Devices
 {
     internal struct OpenVRInputControllerState : IInputStateTypeInfo
     {
-        [InputControl(layout = "Integer")]
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", alias = "systemButton", usage = "MenuButton")]
+        public bool system;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", usage = "MenuTouch")]
+        public bool systemTouched;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", usage = "PrimaryButton")]
+        public bool primaryButton;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", usage = "PrimaryTouch")]
+        public bool primaryTouched;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", usage = "SecondaryButton")]
+        public bool secondaryButton;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", usage = "SecondaryTouch")]
+        public bool secondaryTouched;
+
+        [InputControl(layout = "Axis", aliases = new string[] { "GripAxis", "squeeze" }, usage = "Grip")]
+        public float grip;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", aliases = new string[] { "GripButton", "squeezeClicked" }, usage = "GripButton")]
+        public bool gripPressed;
+
+        [InputControl(layout = "Axis", alias = "squeezeForce", usage = "GripForce")]
+        public float gripForce;
+
+        [InputControl(layout = "Axis", usage = "Trigger")]
+        public float trigger;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", usage = "TriggerButton")]
+        public bool triggerPressed;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", usage = "TriggerTouch")]
+        public bool triggerTouched;
+
+        [InputControl(layout = "Vector2", aliases = new string[] { "joystick", "Primary2DAxis" }, usage = "Primary2DAxis")]
+        public Vector2 thumbstick;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", alias = "joystickClicked", usage = "Primary2DAxisClick")]
+        public bool thumbstickClicked;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", alias = "joystickTouched", usage = "Primary2DAxisTouch")]
+        public bool thumbstickTouched;
+
+        [InputControl(layout = "Vector2", aliases = new string[] { "touchpad", "Secondary2DAxis" }, usage = "Secondary2DAxis")]
+        public Vector2 trackpad;
+
+        [MarshalAs(UnmanagedType.I1)]
+        [InputControl(layout = "Button", alias = "touchpadTouched", usage = "Secondary2DAxisTouch")]
+        public bool trackpadTouched;
+
+        [InputControl(layout = "Axis", alias = "touchpadForce", usage = "Secondary2DAxisForce")]
+        public float trackpadForce;
+
+        [InputControl(layout = "Pose", aliases = new string[] { "device", "gripPose" }, usage = "Device")]
+        public PoseState devicePose;
+
+        [InputControl(layout = "Pose", alias = "aimPose", usage = "Pointer")]
+        public PoseState pointer;
+
+        // trackingState and isTracked MUST be below PoseStates or else their offsets in the struct get overwritten
+        [InputControl(layout = "Integer", usage = "IsTracked")]
         public InputTrackingState trackingState;
 
         [MarshalAs(UnmanagedType.I1)]
-        [InputControl(layout = "Button")]
+        [InputControl(layout = "Button", usage = "TrackingState")]
         public bool isTracked;
 
-        [InputControl(layout = "Vector3")]
+        [InputControl(layout = "Vector3", alias = "gripPosition")]
         public Vector3 devicePosition;
 
-        [InputControl(layout = "Quaternion")]
+        [InputControl(layout = "Quaternion", alias = "gripOrientation")]
         public Quaternion deviceRotation;
+
+        [InputControl(layout = "Vector3")]
+        public Vector3 pointerPosition;
+
+        [InputControl(layout = "Quaternion", alias = "pointerOrientation")]
+        public Quaternion pointerRotation;
 
         public FourCC format => new('O', 'V', 'R', 'C');
     }

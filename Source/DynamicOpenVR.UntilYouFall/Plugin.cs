6b97e1ac54413851f89d38627f3914223c261d02
Nicolas Gnyra <nicolas@gnyra.com>
2024-12-18 11:23:00 -0500

    Update UYF

--- Source/DynamicOpenVR.UntilYouFall/Plugin.cs
+++ Source/DynamicOpenVR.UntilYouFall/Plugin.cs
@@ -1,20 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Il2CppInterop.Runtime;
 using Il2CppInterop.Runtime.Injection;
+using SG.Claymore;
 using UnityEngine;
+using UnityEngine.LowLevel;
+using UnityEngine.Rendering;
 
 namespace DynamicOpenVR.UntilYouFall
 {
     [BepInPlugin("com.nicoco007.until-you-fall.dynamicopenvr", "DynamicOpenVR.UntilYouFall", "1.0.0")]
     public class Plugin : BasePlugin
     {
+        internal static Plugin instance { get; private set; }
+
+        public Plugin()
+            : base()
+        {
+            instance = this;
+        }
+
         public override void Load()
         {
-            ClassInjector.RegisterTypeInIl2Cpp<OpenVRActionManager>();
-            // ClassInjector.RegisterTypeInIl2Cpp<OpenVREventHandler>();
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 
             try
             {
@@ -28,7 +43,39 @@ namespace DynamicOpenVR.UntilYouFall
             }
 
             Logging.Logger.handler = new BepInExLoggerWrapper(Log);
-            OpenVRActionManager.instance.Initialize("Until You Fall", Path.Combine(Paths.GameRootPath, "actions.json"));
+            OpenVRActionManager.instance.Configure("Until You Fall", Path.Combine(Paths.GameRootPath, "actions.json"));
+            OpenVRActionManager.instance.Start();
+        }
+
+        private class OpenVRActionManagerUpdate : Il2CppSystem.Object { }
+
+        [HarmonyPatch(typeof(ClaymorePlayerLoopInitializer), nameof(ClaymorePlayerLoopInitializer.Init))]
+        private static class ClaymorePlayerLoopInitializer_Init
+        {
+            private static void Postfix()
+            {
+                ClassInjector.RegisterTypeInIl2Cpp<OpenVRActionManagerUpdate>();
+
+                var mySystem = new PlayerLoopSystem
+                {
+                    type = Il2CppType.Of<OpenVRActionManagerUpdate>(),
+                    updateDelegate = (PlayerLoopSystem.UpdateFunction)(() => OpenVRActionManager.instance.Update(UpdateType.Dynamic)),
+                };
+
+                PlayerLoopSystem playerLoop = PlayerLoop.GetCurrentPlayerLoop();
+                PlayerLoopSystem[] systems = playerLoop.subSystemList;
+                int index = Array.FindIndex(systems, s => s.type == Il2CppType.Of<UnityEngine.PlayerLoop.EarlyUpdate>());
+
+                List<PlayerLoopSystem> subsystems = new(systems[index].subSystemList);
+                subsystems.Insert(0, mySystem);
+
+                systems[index].subSystemList = subsystems.ToArray();
+                playerLoop.subSystemList = systems;
+
+                PlayerLoop.SetPlayerLoop(playerLoop);
+
+                RenderPipelineManager.beginCameraRendering += new Action<ScriptableRenderContext, Camera>((ctx, camera) => OpenVRActionManager.instance.Update(UpdateType.BeforeRender));
+            }
         }
 
         private class BepInExLoggerWrapper : Logging.ILogHandler
